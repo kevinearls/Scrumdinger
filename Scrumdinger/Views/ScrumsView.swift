2ac9a13b6a190c711ed5b8774fef2a3dd0aaf66a
Kevin Earls <kevinearls@mac.com>
2023-10-24 14:10:21 +0200

    Added ScrumsView and made it the default
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Kevin Earls on 24/10/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
